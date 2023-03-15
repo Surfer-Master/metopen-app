e6cbf133731ce8370877c191e209d6e82ae564f4
Sky Surfer <athat1256@gmail.com>
2023-03-15 21:57:42 +0700

    second commit

--- conn.php
+++ conn.php
@@ -2,7 +2,7 @@
     $server = "localhost";
     $user = "root";
     $password = "";
-    $database = "project_mikroprosesor";
+    $database = "";
 
     $conn = mysqli_connect($server, $user, $password, $database);
 
