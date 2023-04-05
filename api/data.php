fda5b95dfb8ac6afe4f3f16a6b4dc9f92ddb3f4d
Sky Surfer <athat1256@gmail.com>
2023-04-05 23:09:11 +0700

    add api

---
+++ api/data.php
@@ -0,0 +1,21 @@
+<?php
+
+$data = json_decode(file_get_contents('php://input'), true);
+if (!$data == NULL) {
+
+    # code....
+    print_r($data);
+
+    print("var_dump");
+    var_dump($data);
+    $sensor = $data['sensor'];
+    $nilai = $data['nilai'];
+
+    print(file_get_contents('php://input'));
+}
+
+
+if (isset($_POST['FingerID']) && isset($_POST['device_id'])) {
+    $fingerID = $_POST['FingerID'];
+    $device_id = $_POST['device_id'];
+}
